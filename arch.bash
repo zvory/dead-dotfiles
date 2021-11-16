39dd3baeb1b5adb983d2637241ee7a4baeeec500
Alex Zvorygin <grafetu@gmail.com>
2021-11-15 21:22:12 -0800

    basics

---
+++ arch.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman -S nvim fzf zsh tailscale sudo openssh tailscale 
+
+# set up systemd-resolveconf
+# do the entire arch install process
+# with customizable hostname
